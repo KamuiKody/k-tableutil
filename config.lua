6ac871a586378039169511ddecf5bb7545e0cd8f
KamuiKody <cfurr17@gmail.com>
2023-03-15 16:31:50 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.Framework = 'standalone' --qbcore-- will give you qbcore notify that is the only other option here
+Config.Framework = 'qbcore' --qbcore-- will give you qbcore notify that is the only other option here
 Config.Start = 'tablestart'
 Config.Vector3 = 'v3'
 Config.Vector4 = 'v4'
